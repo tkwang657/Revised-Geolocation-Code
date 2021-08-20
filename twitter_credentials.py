e39d2dc9b81b0bdc35dfd28491e6ba47b781c295
tkwang657 <alex.kwang@yale.edu>
2021-08-20 17:51:37 -0400

    Uploaded code

---
+++ twitter_credentials.py
@@ -0,0 +1,9 @@
+################################################################################
+##################### Fill up you Twitter Credentials ##########################
+twitter_auth = {
+    'consumer_key': 'token',
+    'consumer_secret': 'key',
+    'access_token': 'token',
+    'access_token_secret': 'key',
+  }
+
